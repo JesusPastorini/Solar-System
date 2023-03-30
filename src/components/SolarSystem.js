9f7a2509db0dff06e3534bc46398e3edcb391ce7
JesusPastorini <juniorpastorini_@hotmail.com>
2023-03-30 18:07:45 -0300

    requisito 4

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Title from './Title';
 
 class SolarSystem extends Component {
   render() {
+    const titulo = 'Planetas'
     return (
-      <div data-testid="solar-system">SolarSystem</div>
-    )
+      <div data-testid="solar-system">
+        <Title headline={titulo}/>
+      </div>
+    );
   }
 }
 
-export default SolarSystem
+export default SolarSystem;
